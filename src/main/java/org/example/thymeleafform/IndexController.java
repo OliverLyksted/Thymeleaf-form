7d30aeb2db96ccce4db8badb0932620e6e347d73
OliverLyksted <olly0001@stud.kea.dk>
2024-02-15 14:19:02 +0100

    Initial commit

---
+++ src/main/java/org/example/thymeleafform/IndexController.java
@@ -0,0 +1,24 @@
+package org.example.thymeleafform;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("userForm", new User());
+        return "index";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user, Model model){
+        System.out.println(user.toString());
+        model.addAttribute("userForm", new User());
+        model.addAttribute("message", "you have registered");
+        return "index";
+    }
+}
---
+++ src/main/java/org/example/thymeleafform/User.java
@@ -0,0 +1,65 @@
+package org.example.thymeleafform;
+
+import java.util.Date;
+import java.time.LocalDate;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String dob;
+
+
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", dob='" + dob + '\'' +
+                '}';
+    }
+}
